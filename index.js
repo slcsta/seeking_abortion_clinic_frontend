8220f5a7f377c7541e31bb7504a3d4010d148ca3
Sarah <sarahlchavez@gmail.com>
2021-10-05 05:33:17 -0700

    created instance function getClinics with fetch request & called it in index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // create variables here
 // base URL
 const port = 'http://localhost:3000';
+// make call object to make fetch requests on - and make instance of ClinicApi with argument of port
+const clinicCall = new ClinicApi(port);
+
+// taking clinicCall and calling getClinicss on it and then invoking it
+clinicCall.getComments()
--- src/clinic_api.js
+++ src/clinic_api.js
@@ -3,6 +3,12 @@ class ClinicApi {
     // making my API objects here - just need 1 object and one object will make all of our api requests
     // pass in 
     constructor(port){
-        this.port
+        this.port = port
+    }
+    // make an instance function - need 1 instance to call these methods on it
+    getClinics(){
+        fetch(this.port + `/clinics`)
+        .then(response => response.json())
+        .then(data => console.log(data))
     }
 }
